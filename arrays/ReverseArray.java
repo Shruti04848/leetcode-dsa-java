1e3b3fe9a8e7317c9af45232aaff23cfc1e5b32a
Shruti <saxenashruti048@gmail.com>
2026-09-14 21:16:40 +0530

    finding duplicate elements in sorted array

---
+++ arrays/ReverseArray.java
@@ -0,0 +1,27 @@
+public class ReverseArray 
+{
+    public static void reverseArray(int[] arr) {
+        int start = 0;
+        int end = arr.length - 1;
+
+        while (start < end) {
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+
+            start++;
+            end--;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {10, 20, 30, 40, 50};
+
+        reverseArray(arr);
+
+        System.out.print("Reversed Array: ");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+}
